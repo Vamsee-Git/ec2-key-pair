4d059bf27dc7641f5986dfd8eed4346323b909a9
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-20 23:43:01 +0530

    Create main.tf

---
+++ Infra/modules/key_pair/main.tf
@@ -0,0 +1,24 @@
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "my-key"
+  public_key = tls_private_key.example.public_key_openssh
+}
+
+resource "local_file" "private_key" {
+  content  = tls_private_key.example.private_key_pem
+  filename = "${path.module}/private_key.pem"
+}
+
+output "private_key_path" {
+  value = local_file.private_key.filename
+}
+
+
+output "private_key_pem" {
+  value = tls_private_key.example.private_key_pem
+  sensitive = true
+}
