c3aba4db4d7ed7d4023245f18ea2a28c259a7999
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-20 23:44:42 +0530

    Update main.tf

--- Infra/modules/ec2/main.tf
+++ Infra/modules/ec2/main.tf
@@ -1,17 +1,7 @@
-resource "tls_private_key" "example" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "deployer" {
-  key_name   = "my-key"
-  public_key = tls_private_key.example.public_key_openssh
-}
-
 resource "aws_instance" "web" {
   ami               = var.ami
   instance_type     = var.instance_type
-  key_name          = aws_key_pair.deployer.key_name
+  key_name          = var.key_name
   subnet_id         = var.subnet_id
   security_groups   = [var.security_group_id]
   associate_public_ip_address = true
@@ -29,16 +19,6 @@ resource "aws_instance" "web" {
   }
 }
 
-resource "local_file" "private_key" {
-  content  = tls_private_key.example.private_key_pem
-  filename = "${path.module}/private_key.pem"
-}
-
-output "private_key_path" {
-  value = local_file.private_key.filename
-}
-
-
 output "instance_id" {
   value = aws_instance.web.id
 }
@@ -47,7 +27,3 @@ output "public_ip" {
   value = aws_instance.web.public_ip
 }
 
-output "private_key_pem" {
-  value = tls_private_key.example.private_key_pem
-  sensitive = true
-}
